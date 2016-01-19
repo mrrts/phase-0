7cf076e0dadbfde39ee1a36c2cfcd2dc7cbd4c03
Ryan Smith <mrryantsmith@gmail.com>
2016-01-19 13:40:08 -0600

    Finish challenges in 4.3

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,11 @@
+
+# Define an Empty Method
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def do_something(name, age, occupation)
+
+end
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,16 @@
+puts "First Name?"
+first = gets.chomp
+
+puts "Middle Name?"
+middle = gets.chomp
+
+puts "Last Name?"
+last = gets.chomp
+
+puts "Hello, #{first} #{middle} #{last}!"
+
+
+puts "Favorite Number?"
+fav = gets.chomp
+
+puts "#{fav} is a nice number, but #{fav.to_i + 1} is even bigger and better!"
