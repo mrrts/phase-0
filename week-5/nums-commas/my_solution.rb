c428e337e09817f7fbda7294acfb480871739491
Ryan Smith <mrryantsmith@gmail.com>
2016-01-27 21:48:03 -0600

    Combine challenges

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,3 +1,35 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+
+# What is the input? Integer of any size
+# What is the output? (i.e. What should the code return?) It should return the integer as a string with all the appropriately placed commas
+# What are the steps needed to solve the problem?
+
+  # Split digits into array
+  # Reverse the order of the digits so that we can begin iteration with the ones place
+  # Make an array to hold the digits and any inserted commas, still in reversed order
+  # Iterate through the digits starting with the ones place
+  # Use each_with_index to keep track of the current index in order to test which place we're on 
+  # Push the digit into the final result array
+  # Insert a comma every three places 
+  #
+  # Test if a) the place (index + 1) is divisible by 3
+  # and b) that there is a digit that exists at the index beyond this one
+  # If both are true, insert a comma
+  # Return the result array, but reverse it back to normal, 
+  # And join the digits and commas back into a single string
+
+  
+# 1. Initial Solution
+
+
 def separate_comma(num)
   # Split digits into array
   # Reverse the order of the digits so that we can begin iteration with the ones place
@@ -30,3 +62,16 @@ def separate_comma(num)
 end
 
 p separate_comma(10000000000000009880000)
+
+
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
+
+
+
+
