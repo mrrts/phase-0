38d7c02196cd190a04be93028f60287e51bd76f1
Ryan Smith <mrryantsmith@gmail.com>
2016-02-02 08:31:29 -0600

    Complete 6.2 Reflection

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -66,4 +66,34 @@ end
 
 
 
-# Reflection
+## Reflection
+
+
+# --- What were the main differences between this die class and the last one you created in terms of
+#  implementation? Did you need to change much logic to get this to work?
+
+# There was hardly any difference, but I did use the .map method to convert all of the values into strings and 
+# check if the argument was an array. There was no other difference, except that this die class accepts a sides 
+# array with only one value, whereas the other did not.
+
+
+# --- What does this exercise teach you about making code that is easily changeable or modifiable? 
+
+# I know that using the class will help me to maintain DRY code, and so I won't have to define these methods 
+# every time I want to find the values of a new die with a different number of sides. I also see that a single,
+# sufficiently abstracted class can be used in several ways without much modification. If the underlying
+# principle is the same, well-abstracted code can be reused, simply passing new initialization values,
+#  which saves a lot of time.  
+
+
+# --- What new methods did you learn when working on this challenge, if any?
+
+# During peer reviews for the previous Die class challenge, I found a solution using the .sample method, which
+# accomplishes the random-selection functionality with a single, succint method call. 
+
+
+# --- What concepts about classes were you able to solidify in this challenge?
+
+# In my initial solution I used a range to generate my random number, and I used the return value of the class's
+# .sides method to define the range's upper bound. This served as a practical illustration of how a class method
+# can use other class methods. 
