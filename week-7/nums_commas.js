fb73e14b166750a6d00859a6425e4eb51d757699
Ryan Smith <mrryantsmith@gmail.com>
2016-02-11 11:57:09 -0600

    Complete 7.5 Reflection

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -79,3 +79,29 @@ console.log(commas(3000));
 
 
 // Reflection
+
+// ----What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+
+// We were not able to rely on a built-in reverse method like Ruby has, so we needed to consider the logical steps included
+// in a more manual reversal of a string. Also, Ruby had a variety of convenient iterator methods, while the js for loops 
+// are a bit cumbersome and ugly in comparison, in my opinion. 
+
+
+// ----What did you learn about iterating over arrays in JavaScript?
+
+// We did not split the string into an array because the strings themselves have indexes, and individual characters can be 
+// read using the string charAt method, so in this exercise we actually learned about iterating over strings. 
+
+
+// ----What was different about solving this problem in JavaScript?
+
+// Similar logic but different syntax and methods... and again, we needed to define our own string-reversal method. 
+
+
+// ----What built-in methods did you find to incorporate in your refactored solution?
+
+// We used the charAt method to access string characters by index. We did not search for a built-in reverse method. We have 
+// also incorporated the toString method and the length method, but these were part of the initial solution already. The 
+// refactor phase was less about incorporating new methods (mostly due to assuming that they did not exist to the extent
+//   that they do in Ruby), and more about DRY-ing the code by making a reverse method and reducing some redundancy in 
+// variable declarations. We also eliminated an unneeded if branch at the beginning of the function. 
