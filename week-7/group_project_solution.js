f59ca2bc72d11657a0f89130ade69b2a87571fd1
Ryan Smith <mrryantsmith@gmail.com>
2016-02-11 00:42:48 -0600

    Complete my Person 2 step

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,51 @@
+// PERSON 2: 
+
+// (Pseudocode based on only reading Person 1's User stories)
+
+
+
+
+// Define an array filled with only even numbers of your choosing
+// Define an array filled with only odd numbers of your choosing
+
+
+// Create a function named sum that accepts an array of integers as an argument
+// and Returns the sum of the integers in the array
+// 	- Declare a new variable as the running sum, set equal to zero
+// 	- FOR each integer in the argument array, add it to the running sum variable
+// 	- Return the running sum variable 
+
+// Call the sum function with the even-numbers array passed in 
+// Call the sum function with the odd-numbers array passed in 
+
+
+
+
+// Create a function named average that accepts an array of integers as an argument 
+// and returns the average of all the integers in the array 
+// 	- Call your new sum function with the argument array passed in to find its sum, and save the sum into a variable 
+// 	- Divide the sum of the argument array by the its length to find the average
+// 	- Return this average 
+
+// Call the average function with the even-numbers array passed in 
+// Call the average function with the odd-numbers array passed in 
+
+
+
+
+// Create a function median that accepts an array of integers as an argument 
+// and returns the median of all the integers in the array 
+// 	- IF the length of the array is even (even number of integers)
+// 		- Find the middlemost pair of numbers by dividing the length by 2 and using the result as the index for one of the numbers and the result-minus-1 as the index for the other
+// 		- Create an array with those two middlemost values
+// 		- Return the average of the two middlemost values (use the previously defined average function)
+// 	- ELSE (the length of the array is odd)
+// 		- Find the middle value by dividing the length by 2 and using the result, rounded down, as the index
+// 		- Return that middle value 
+
+// Call the median function with the even-numbers array passed in 
+// Call the median function with the odd-numbers array passed in 
+
+
+
+
