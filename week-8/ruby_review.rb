8531ef26261155c5355d678770c4532003437dfa
Ryan Smith <mrryantsmith@gmail.com>
2016-02-15 20:47:37 -0600

    Complete 8.6 and Reflection

---
+++ week-8/ruby_review.rb
@@ -0,0 +1,114 @@
+# Cipher Challenge
+
+# I worked on this challenge [by myself, with:]
+# I spent [#] hours on this challenge.
+
+# 1. Solution
+# Write your comments on what each thing is doing.
+# If you have difficulty, go into IRB and play with the methods.
+
+# def dr_evils_cipher(coded_message)
+#   input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the Ruby docs.
+#   decoded_sentence = []
+#   cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
+#             "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
+#             "g" => "c",
+#             "h" => "d",
+#             "i" => "e",
+#             "j" => "f",
+#             "k" => "g",
+#             "l" => "h",
+#             "m" => "i",
+#             "n" => "j",
+#             "o" => "k",
+#             "p" => "l",
+#             "q" => "m",
+#             "r" => "n",
+#             "s" => "o",
+#             "t" => "p",
+#             "u" => "q",
+#             "v" => "r",
+#             "w" => "s",
+#             "x" => "t",
+#             "y" => "u",
+#             "z" => "v",
+#             "a" => "w",
+#             "b" => "x",
+#             "c" => "y",
+#             "d" => "z"}
+
+#   input.each do |x| # What is #each doing here?
+#     found_match = false  # Why would this be assigned to false from the outset? What happens when it's true?
+#     cipher.each_key do |y| # What is #each_key doing here?
+#       if x == y  # What is this comparing? Where is it getting x? Where is it getting y? What are those variables really?
+#         decoded_sentence << cipher[y]
+#         found_match = true
+#         break  # Why is it breaking here?
+#       elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #What the heck is this doing?
+#         decoded_sentence << " "
+#         found_match = true
+#         break
+#       elsif (0..9).to_a.include?(x) # Try this out in IRB. What does   " (0..9).to_a "    do?
+#         decoded_sentence << x
+#         found_match = true
+#         break
+#       end
+#     end
+#     if not found_match  # What is this looking for?
+#       decoded_sentence << x
+#     end
+#   end
+#   decoded_sentence = decoded_sentence.join("")
+#   #What is this method returning?
+# end
+
+# Your Refactored Solution
+
+def dr_evils_cipher (coded_message, offset = 4)
+  decoded_message = ""
+  alpha = ('a'..'z').to_a
+  space_chars = ["@", "#", "$", "%", "^", "&", "*"]
+  coded_message.downcase.each_char do |char|
+    if space_chars.include? char
+      decoded_message << " "
+    elsif alpha.include? char
+      decoded_message << alpha[alpha.index(char) - offset]
+    else
+      decoded_message << char
+    end
+  end
+  return decoded_message
+end
+
+
+
+# Driver Test Code:
+p dr_evils_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver test code and should print true
+# Find out what Dr. Evil is saying below and turn it into driver test code as well. Driver test code statements should always return "true."
+p dr_evils_cipher("syv%ievpc#exxiqtxw&ex^e$xvegxsv#fieq#airx%xlvsykl$wizivep#tvitevexmsrw.#tvitevexmsrw#e*xlvsykl#k&aivi%e@gsqtpixi&jempyvi.
+&fyx%rsa,$pehmiw@erh#kirxpiqir,%ai%jmreppc@lezi&e&asvomrk%xvegxsv#fieq,^almgl^ai^wlepp%gepp@tvitevexmsr^l")
+p dr_evils_cipher("csy&wii,@m'zi@xyvrih$xli*qssr$mrxs&alex@m#pmoi%xs#gepp%e^hiexl#wxev.")
+p dr_evils_cipher("qmrm#qi,*mj^m#iziv^pswx#csy#m^hsr'x%orsa^alex@m%asyph^hs.
+@m'h%tvsfefpc%qszi$sr%erh*kix#ersxliv$gpsri@fyx*xlivi@asyph^fi@e^15&qmryxi@tivmsh%xlivi$alivi*m*asyph&nywx^fi$mrgsrwspefpi.")
+p dr_evils_cipher("alc@qeoi*e$xvmppmsr^alir#ai*gsyph%qeoi...#fmppmsrw?")
+
+# Reflection
+# Keep your reflection limited to 10-15 minutes!
+
+# --- What concepts did you review or learn in this challenge?
+
+# I iterated over a string using each_char for the first time, instead of splitting the string into an array and iterating
+# over the array. I also needed to rely on negative numbers as array indices, since the alphabet array needed to cycle
+# back onto itself if the offset took you further back than the letter 'a'. 
+
+
+# --- What is still confusing to you about Ruby?
+
+# Well, at this point, I do not know what I do not know, but everything in the curriculum for Ruby is pretty clear to 
+#   me at this point. 
+
+
+# --- What are you going to study to get more prepared for Phase 1?
+
+# I am submitting this single review exercise in order to officially finish the assignment, but I hope to find time to do all of the Ruby review challenges and add them into this file. I also hope to study some more Javascript. I would
+#   like to challenge myself to work out some more Javascript challenges, perhaps another game or two. 
